650709d6121e33f2445007edfcb77267fbdad9e7
kimn <a9523023650@gmail.com>
2022-05-14 12:10:52 +0800

    [新增] 配置環境變數

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+export default defineConfig(({ mode }) => {
+    loadEnv(mode, process.cwd());
+    return {
+        plugins: [vue()],
+        resolve: {
+            alias: {
+                "@": resolve(process.cwd(), "src"),
+            },
+        },
+    };
+});
