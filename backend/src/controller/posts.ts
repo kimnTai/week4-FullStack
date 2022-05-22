405687d31ba93089eedc17f2c7552651c16bd2e2
kimn <a9523023650@gmail.com>
2022-05-23 00:33:41 +0800

    [修改] 貼文驗證 id

--- backend/src/controller/posts.ts
+++ backend/src/controller/posts.ts
@@ -27,7 +27,9 @@ class PostsController {
     createPosts = async (req: Request, res: Response): Promise<void> => {
         try {
             const { content, type, user, image } = req.body;
-            await Model.Users.findById(user);
+            if (!(await Model.Users.findById(user))) {
+                throw new Error("無此 id");
+            }
             const result = await Model.Posts.create({ content, type, user, image });
             res.send({ status: "success", result });
         } catch (error: any) {
